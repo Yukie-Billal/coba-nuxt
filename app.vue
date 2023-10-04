aaae450015c4540247a910d4565b8f967a604e92
Yukie M Billal <yukiembillal01@gmail.com>
2023-10-04 13:53:45 +0700

    feat: hello world

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup>
+import './assets/app.css'
+import Main from './components/Main.vue'
+const message = "Hello, Yukie muhammad billal"
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+      <Main :message="message" />
   </div>
 </template>
+
+<style scoped>
+div {
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   background: #171616;
+}
+</style>
---
+++ components/Main.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+   name: 'Main',
+   props: ['message']
+}
+</script>
+
+<template>
+   <h1>{{message}}</h1>
+</template>
+
+<style scoped>
+h1 {
+   font-family: sans-serif;
+   font-size: 44px;
+   font-weight: 500;
+   color: white;
+}
+</style>
